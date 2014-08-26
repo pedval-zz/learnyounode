bda6772dfb8b2a56b364e75365fe77878dda9fe5
Pedro <pedro.valencia@thomascookonline.com>
2014-08-26 17:21:32 +0100

    Exercise 11

--- module 1/exercise11.js
+++ module 1/exercise11.js
@@ -1,1 +1,13 @@
-var http = require('http');
+var http = require('http'),
+	fs = require('fs');
+
+var server = http.createServer(function(req, res){	
+
+	var readStream = fs.createReadStream(process.argv[3]);
+
+	readStream.on('open', function () {    
+    	readStream.pipe(res);
+  	});
+})
+
+server.listen(process.argv[2]);
